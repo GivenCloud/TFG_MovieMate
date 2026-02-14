ce085463bada30e82ef135273ef97824b0f8567d
GivenCloud <christian15.cmc@gmail.com>
2026-02-14 20:12:17 +0100

    Añadido Docker

--- MovieMate/moviemate-backend/src/main/java/com/moviemate/config/SecurityConfig.java
+++ MovieMate/moviemate-backend/src/main/java/com/moviemate/config/SecurityConfig.java
@@ -66,6 +66,7 @@ public class SecurityConfig {
                                  "/v3/api-docs/**",
                                  "/swagger-resources/**",
                                  "/webjars/**",
+                                 "/actuator/**",
                                  "/api/auth/**",
                                  "/api/content/**",
                                  "/api/lists/public",
